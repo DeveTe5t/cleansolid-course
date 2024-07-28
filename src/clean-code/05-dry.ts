bd973e9f3a51d6d8a25f313000e888bb63f6bb9a
DeveTe5t <devcurses@gmail.com>
2024-07-27 22:06:31 -0600

    Excercise 5 finish

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -9,11 +9,33 @@ class Product {
         public size: SizeProduct = '',
     ) { }
 
+    isProductReady(): boolean {
+
+        for (const key in this) {
+            switch (typeof this[key]) {
+                case 'string':
+                    if ((this[key] as string).length <= 0) throw new Error(`${key} is empty`);
+                    // if (this[key].length <= 0) throw new Error(`${key} is empty`);
+                    break;
+                case 'number':
+                    if ((this[key] as number) <= 0) throw new Error(`${key} is zero`);
+                    // if (this[key] <= 0) throw new Error(`${key} is zero`);
+                    break;
+                default:
+                    throw Error(`${typeof this[key]} is not supported`);
+            }
+        }
+
+        return true;
+    }
+
     toString() {
         // No dry
-        if (this.name.length <= 0) throw new Error('Name is empty');
-        if (this.price <= 0) throw new Error('Price is zero');
-        if (this.size.length <= 0) throw new Error('Size is empty');
+        // if (this.name.length <= 0) throw new Error('Name is empty');
+        // if (this.price <= 0) throw new Error('Price is zero');
+        // if (this.size.length <= 0) throw new Error('Size is empty');        
+
+        if (!this.isProductReady()) return;
 
         return `${this.name} (${this.price}) ${this.size}`;
     }
@@ -21,6 +43,7 @@ class Product {
 
 (() => {
 
-    const bluePants = new Product('Blue Large Pants', 10, 'M');
+    const bluePants = new Product('Blue Pants', 12, 'L');
+    // const bluePants = new Product('Blue Large Pants', 10, 'M');
     console.log(bluePants.toString());
 })();
