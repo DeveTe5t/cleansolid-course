4fd3ed0c71a90aea3ded947ccd3defee17fe59e6
DeveTe5t <devcurses@gmail.com>
2024-07-27 20:13:26 -0600

    Finish Excercise 3 part 1

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,36 +1,69 @@
 (() => {
 
     // función para obtener información de una película por Id
-    function getAllMovies(movieId: string) {
-        console.log({ movieId });
+    function getMovieById(id: string) {
+        console.log({ id });
     }
 
     // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getAllMovieActors(id: string) {
+    function getMovieCastById(id: string) {
         console.log({ id });
     }
 
     // funcion para obtener el bio del actor por el id
-    function getUsuario(ActorId: string) {
-        console.log({ ActorId });
+    function getActorBioById(id: string) {
+        console.log({ id });
     }
 
     // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[]) {
+    interface Movie {
+        cast: string[];
+        description: string;
+        rating: number;
+        title: string;
+    }
+
+    function createMovie({ title, description, rating, cast }: Movie) {
         console.log({ title, description, rating, cast });
     }
 
     // Crea un nuevo actor
-    function createActorIfActorNotExists(fullName: string, birthdate: Date): boolean {
+    function checkFullName(fullName: string) {
+        console.log({ fullName });
+        return true;
+    }
+
+    function createActor(fullName: string, birthdate: Date): boolean {
 
         // tarea asincrona para verificar nombre
         // ..
         // ..
-        if (fullName === 'fernando') return false;
+        // if (fullName === 'fulano') return false;
+        if (checkFullName(fullName)) return false;
 
-        console.log('Crear actor');
+        console.log('Crear actor', birthdate);
         return true;
 
+    }
+
+    // Continue
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        let result;
+        if (isDead) {
+            result = 1500;
+        } else {
+            if (isSeparated) {
+                result = 2500;
+            } else {
+                if (isRetired) {
+                    result = 3000;
+                } else {
+                    result = 4000;
+                }
+            }
+        }
+
+        return result;
     }
 
 })();
