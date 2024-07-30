75587edb6052df1c16be6e89f7051151c13f8601
DeveTe5t <devcurses@gmail.com>
2024-07-29 21:50:16 -0600

    Exercise 6 continue: Not applying the single reponsability principle

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,5 +1,7 @@
 (() => {
 
+    // Not applying the single responsibility principle
+
     type Gender = 'M' | 'F';
 
     class Person {
@@ -25,7 +27,53 @@
         }
     }
 
-    const newPerson = new Person('Fulano', 'M', new Date('1980-12-12'));
-    console.log({ newPerson });
+    // const newPerson = new Person('Fulano', 'M', new Date('1980-12-12'));
+    // console.log({ newPerson });
+
+    class User extends Person {
+        public lastAccess: Date;
+
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date,
+        ) {
+            super(name, gender, birthdate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials(): boolean {
+            return true;
+        }
+    }
+
+    class UserSetting extends User {
+
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date,
+        ) {
+            super(email, role, name, gender, birthdate);
+        }
+    }
+
+    const newUserSetting = new UserSetting(
+        '/home/user',
+        '/home/user/documents',
+        'fulan@gmail.com',
+        'admin',
+        'Fulano',
+        'M',
+        new Date('1980-12-12'),
+    );
+
+    console.log({ newUserSetting, areCredentialsValid: newUserSetting.checkCredentials() });
 
 })();
